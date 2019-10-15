d814b21e78f054503ca6b54a0ffe983f31a1a590
kyo199 <kyo199@nate.com>
2019-10-15 17:10:50 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+exports.handler = async (event, context, callback) => {
+    const operation = event.httpMethod;
+    switch (operation) {
+        case 'GET':
+            let data = {
+                'id': 1,
+                'name': 'changhee'
+            }
+            callback(null, {
+                'statusCode': 200,
+                'headers': {},
+                'body': JSON.stringify(data)
+            });
+            break;
+        case 'POST':
+            callback(null, {
+                'statusCode': 200
+            });
+            break;
+        default:
+            callback(new Error(`Operation Error "${operation}"`))
+    }
+};
